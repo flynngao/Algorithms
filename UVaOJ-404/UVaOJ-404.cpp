965c4057c2413ca2922112ab74990540acfa9b89
linzhi.gao <linzhi.gao@alipay.com>
2014-05-16 15:32:54 +0800

    404

---
+++ UVaOJ-404/UVaOJ-404.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+
+
+#define MAXS 1000
+
+char s[MAXS];
+
+char fc[MAXS];
+
+int num;
+
+int ispf(char* string){
+
+    
+    for(int i = 0;i < num/2;i++){
+        if(string[i] !=  string[num - i - 1]){
+            return 0;     
+        }
+    }
+    return 1;
+}
+int ismf(char* string){
+
+    for(int i = 0;i < num/2;i++){
+        int j = fc[string[i]] != string[num - i - 1];
+
+        printf("%d\n", j);
+        if((fc[string[i]] && (fc[string[i]] != string[num - i - 1])) || (string[i] != string[num - i - 1])){
+
+            return 0;     
+        } 
+    }
+    return 1;
+}
+
+int main(){
+
+    int isp = 0;
+    int ism = 0;
+
+    fc['A'] = 'A';
+    fc['E'] = '3';
+    fc['H'] = 'H';
+    fc['I'] = 'I';
+    fc['J'] = 'L';
+    fc['L'] = 'J';
+    fc['M'] = 'M';
+    fc['O'] = 'O';
+    fc['S'] = '2';
+    fc['T'] = 'T';
+    fc['U'] = 'U';
+    fc['V'] = 'V';
+    fc['W'] = 'W';
+    fc['X'] = 'X';
+    fc['Y'] = 'Y';
+    fc['Z'] = '5';
+    fc['1'] = '1';
+    fc['2'] = 'S';
+    fc['3'] = 'E';
+    fc['5'] = 'Z';
+    fc['8'] = '8';
+
+    while(scanf("%s", s)!=EOF){
+
+        num = strlen(s);
+        isp=ispf(s);
+        ism=ismf(s);
+        if(isp && ism)
+        {
+            printf("%s -- is a mirrored palindrome.\n", s);
+        }
+        else if(isp)
+        {
+            printf("%s -- is a regular palindrome.\n", s);
+        }
+        else if(ism)
+        {
+            printf("%s -- is a mirrored string.\n", s);
+        }
+        else
+        {
+            printf("%s -- is not a palindrome.\n", s);
+        }
+        printf("\n");
+    }
+         
+}
